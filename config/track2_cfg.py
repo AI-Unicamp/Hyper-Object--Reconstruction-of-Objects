325e231f1ba3c9b9d91d8f863df39d293ff53df6
Jasmine Battestin Nunes <jasmine.battestin@gmail.com>
2025-11-11 01:01:39 -0300

    alteracoes minimas no tutorial

--- config/track2_cfg.py
+++ config/track2_cfg.py
@@ -1,12 +1,12 @@
 import numpy as np 
 
 # data parameters
-crop_size = 32 #320
+crop_size = 320 # valor original: 320
 
 # data info
 bands = np.arange(400, 1000, 10)
 
 # training parameters
-batch_size = 1 # valor original: 2
-epochs = 1  # valor original: 14
+batch_size = 2 # valor original: 2
+epochs = 14  # valor original: 14
 init_lr = 4e-4
