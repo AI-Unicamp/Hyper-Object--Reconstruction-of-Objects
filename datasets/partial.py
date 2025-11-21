5985db40753ca794645497f58d1c6587aceba386
lambdcalculus <lambdcalcullus@gmail.com>
2025-11-21 17:42:40 -0300

    attempt to implement faster training

---
+++ datasets/partial.py
@@ -0,0 +1,122 @@
+from __future__ import annotations
+from pathlib import Path
+from typing import Any, Literal, Optional
+
+from torch.utils.data import Dataset
+import zarr
+import numpy as np
+import torch
+import os
+
+from .io import read_rgb_image, read_h5_cube, read_mosaic
+
+# deterministic transforms - allows transforms to remain consistent in input/output even
+# in separate dataloaders
+class DeterministicTransforms:
+    def __init__(self, track: Literal[1, 2], is_out: bool, base_seed: int = 0):
+        self.base_seed = int(base_seed)
+        self.track = track
+        self.is_out = is_out
+        self.transforms: dict[str, dict[str, Any]] = {}
+
+    def add_transform(self, transform_name: str, **kwargs):
+        self.transforms[transform_name] = kwargs
+
+    def set_epoch(self, epoch: int):
+        self.epoch = int(epoch)
+
+    def _rng_for(self, idx: int):
+        # combine base_seed, epoch, and idx into a 32-bit seed
+        # using a simple mixing (avoid collisions)
+        h = (self.base_seed * 1000003) ^ (self.epoch * 9176) ^ idx
+        seed = h & 0xFFFFFFFF
+        return np.random.default_rng(seed)  # or random.Random(seed)
+
+    def _random_crop_det(self, img: torch.Tensor, rng, ps=256):
+        H, W = img.shape[-2], img.shape[-1]
+        if self.track==2 and self.is_out:
+            # make sure the RNG call is exactly the same as for the input
+            r = int(rng.integers(0, H//2 - ps))
+            c = int(rng.integers(0, W//2 - ps))
+        else:
+            r = int(rng.integers(0, H - ps))
+            c = int(rng.integers(0, W - ps))
+
+        if self.track == 1:
+            # ensure coordinates are even; i.e. crop aligns with bayer pattern
+            r -= r % 2
+            c -= c % 2
+        if self.track == 2 and self.is_out:
+            # output is twice the size of the input
+            img = img[:, 2*r:2*r+2*ps, 2*c:2*c+2*ps]
+        else:
+            img = img[:, r:r+ps, c:c+ps]
+
+        return img
+
+    def _random_flip_det(self, img: torch.Tensor, rng):
+        if rng.random() < 0.5:
+            img = img.flip(dims=[2])
+        return img
+
+    def apply(self, img, idx: int):
+        rng = self._rng_for(idx)
+
+        if "random_crop" in self.transforms:
+            img = self._random_crop_det(img, rng, **self.transforms["random_crop"])
+        if "random_flip" in self.transforms:
+            img = self._random_flip_det(img, rng, **self.transforms["random_flip"])
+
+        return img
+
+class PartialDataset(Dataset):
+    def __init__(
+        self,
+        data_root: str,
+        track: Literal[1, 2],
+        dataset_type: Literal["train", "test-public"],
+        img_type: Literal["hsi_61", "hsi_61_zarr", "mosaic", "rgb_2", "rgb"],
+        transforms: Optional[DeterministicTransforms] = None,
+    ) -> None:
+        super().__init__()
+        self.track = track 
+        self.img_type = img_type
+
+        path = f"{data_root}/track{track}/{dataset_type}/{img_type}"
+        self.paths = [Path(f"{path}/{f}") for f in os.listdir(path)]
+
+        self.zarr_cache: list[zarr.Array | zarr.Group | None] = [None] * len(self.paths)
+
+        self.transforms = transforms
+
+    def __len__(self) -> int:
+        return len(self.paths)
+
+    def _read_zarr(self, idx: int):
+        if self.zarr_cache[idx] is None:
+            self.zarr_cache[idx] = zarr.open(str(self.paths[idx]), mode='r')
+        return self.zarr_cache[idx]["data"][:]
+
+    def __getitem__(self, idx: int):
+        path = self.paths[idx]
+        out = torch.tensor([])
+        match self.img_type:
+            case "hsi_61":
+                cube = read_h5_cube(path, 'cube')                          # (H,W,C)
+                out = torch.from_numpy(np.transpose(cube, (2, 0, 1)))    # C,H,W
+            case "hsi_61_zarr":
+                cube = self._read_zarr(idx)                          # (H,W,C)
+                out = torch.from_numpy(cube)    # C,H,W
+            case "mosaic":
+                mosaic = read_mosaic(path)                                  # (H,W,1) float32 [0,1]
+                out = torch.from_numpy(np.transpose(mosaic, (2, 0, 1)))    # 1,H,W
+            case "rgb_2":
+                rgb_2 = read_rgb_image(path)                             # (H,W,3) float32 [0,1]
+                out = torch.from_numpy(np.transpose(rgb_2, (2, 0, 1)))  # C,H,W
+            case "rgb":
+                rgb = read_rgb_image(path)                             # (H,W,3) float32 [0,1]
+                out = torch.from_numpy(np.transpose(rgb, (2, 0, 1)))  # C,H,W
+
+        if self.transforms is not None:
+            out = self.transforms.apply(out, idx)
+        return out
---
+++ train_fast.py
@@ -0,0 +1,184 @@
+import torch
+import wandb
+import yaml
+import os
+import argparse
+from datetime import datetime
+
+from torch.utils.data import DataLoader
+from trainer.losses import ReconLoss
+from trainer.trainer import Trainer, TrainerCfg
+from utils.tools_wandb import ToolsWandb
+
+from datasets.partial import DeterministicTransforms
+from datasets.transform import random_crop, random_flip
+
+from models import setup_model
+
+from typing import Dict, Any
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--track", type=int, required=True, help="track to run")
+parser.add_argument("-d", "--data_dir", type=str, default="./data", required=False, help="path to dataset directory")
+parser.add_argument("-c", "--config", type=str, required=False, help="path of config file to use (defaults to baselines for each track)")
+
+# TODO: implement
+# parser.add_argument("--continue_from", type=str, required=False, help="checkpoint to start from")
+parser.add_argument("--use_wandb", default=False, required=False, help="log to wandb", action="store_true")
+
+args = parser.parse_args()
+
+if args.config is None:
+    if args.track == 1:
+        config_path = "config/raw2hsi_baseline.yaml"
+    elif args.track == 2:
+        config_path = "config/mst_plus_plus_up_baseline.yaml"
+    else:
+        raise ValueError(f"'{args.track}' is invalid value for track: must be 1 or 2.")
+else:
+    config_path = args.config
+
+config: Dict[str, Any]
+with open(config_path, "r") as file:
+    config = yaml.load(file, Loader=yaml.FullLoader)
+
+if "model" not in config:
+    raise ValueError("No model settings found.")
+elif "model_name" not in config["model"]:
+    raise ValueError("No model name found.")
+
+model_config = config["model"]
+train_config = config.get("train", {})
+transforms_config = config.get("transforms", {})
+
+print(f"Preparing to train model '{model_config}'...")
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    print("WARNING: GPU not found, using CPU to train.")
+    device = torch.device("cpu")
+
+transforms_in = DeterministicTransforms(track=args.track, is_out=False)
+transforms_out = DeterministicTransforms(track=args.track, is_out=True)
+if transforms_config.get("random_crop", False):
+    transforms_in.add_transform("random_crop", ps=transforms_config.get("crop_size", 320))
+    transforms_out.add_transform("random_crop", ps=transforms_config.get("crop_size", 320))
+if transforms_config.get("random_flip", False):
+    transforms_in.add_transform("random_flip")
+    transforms_out.add_transform("random_flip")
+
+from datasets.partial import DeterministicTransforms, PartialDataset
+
+ds_train_in = PartialDataset(
+    data_root=f"{args.data_dir}",
+    track=args.track,  # 1 for mosaic, 2 for rgb_2
+    dataset_type="train",
+    img_type="mosaic" if args.track==1 else "rgb_2",
+    transforms=transforms_in
+)
+
+ds_train_out = PartialDataset(
+    data_root=f"{args.data_dir}",
+    track=args.track,  # 1 for mosaic, 2 for rgb_2
+    dataset_type="train",
+    img_type="hsi_61_zarr",
+    transforms=transforms_out
+)
+
+ds_val_in = PartialDataset(
+    data_root=f"{args.data_dir}",
+    track=args.track,  # 1 for mosaic, 2 for rgb_2
+    dataset_type="test-public",
+    img_type="mosaic" if args.track==1 else "rgb_2",
+)
+
+ds_val_out = PartialDataset(
+    data_root=f"{args.data_dir}",
+    track=args.track,  # 1 for mosaic, 2 for rgb_2
+    dataset_type="test-public",
+    img_type="hsi_61",
+)
+
+train_loader_in = DataLoader(
+        ds_train_in,
+        batch_size=train_config.get("batch_size_train", 4),
+        num_workers=train_config["fast"].get("num_workers_train_in", 4),
+        shuffle=True,
+        pin_memory=True,
+        persistent_workers=True
+    )
+
+train_loader_out = DataLoader(
+        ds_train_out,
+        batch_size=train_config.get("batch_size_train", 4),
+        num_workers=train_config["fast"].get("num_workers_train_out", 4),
+        shuffle=True,
+        pin_memory=True,
+        persistent_workers=True
+    )
+
+val_loader_in  = DataLoader(
+        ds_val_in,
+        batch_size=train_config.get("batch_size_test", 4),
+        num_workers=train_config["fast"].get("num_workers_test_in", 4),
+        shuffle=False,
+        pin_memory=True,
+        persistent_workers=True,
+    )
+
+val_loader_out  = DataLoader(
+        ds_val_out,
+        batch_size=train_config.get("batch_size_test", 4),
+        num_workers=train_config["fast"].get("num_workers_test_out", 4),
+        shuffle=False,
+        pin_memory=True,
+        persistent_workers=True,
+    )
+
+config_name = os.path.splitext(os.path.basename(config_path))[0]
+run_name = f"{config_name}_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+out_dir = f"runs/track{args.track}/{run_name}"
+cfg = TrainerCfg(
+        out_dir=out_dir,
+        epochs=train_config.get("epochs", 1000),
+        amp=train_config.get("amp", True),
+        optim=train_config.get("optim", "adamw"),
+        lr=train_config.get("lr", 2e-4),
+        weight_decay=train_config.get("weight_decay", 1e-4),
+        scheduler_type=train_config.get("scheduler","cosine"),
+        eta_min=train_config.get("eta_min", 1e-6),
+        lambda_sam=train_config.get("lambda_sam", 0.1),     
+        metrics_report_interval=train_config.get("metrics_report_interval", 5),
+    )
+
+loss_fn = ReconLoss(lambda_sam=cfg.lambda_sam)
+
+model = setup_model(model_config)
+
+run_wandb = None
+flattened_config = {}
+ToolsWandb.config_flatten(config, flattened_config)
+if args.use_wandb:
+    run_wandb = ToolsWandb.init_wandb_run(
+            f_configurations=flattened_config,
+            run_name=run_name
+            )
+
+from trainer.fast_trainer import FastTrainer
+trainer = FastTrainer(
+    model=model,
+    train_loader_in=train_loader_in,
+    train_loader_out=train_loader_out,
+    val_loader_in=val_loader_in,
+    val_loader_out=val_loader_out,
+    loss_fn=loss_fn,
+    cfg=cfg,
+    device=device,
+    wandb_run=run_wandb
+)
+
+try:
+    trainer.fit()
+except KeyboardInterrupt:
+    if run_wandb is not None:
+        run_wandb.finish()
